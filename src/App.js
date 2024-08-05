91f13460ac1a81defc5afe249ae205c8905e48e7
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-08-05 23:08:41 +0300

    added new nav bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Container } from '@mui/material';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DrawerAppBar from './components/Navigation';
 import SampleButton from './components/SampleButton';
+import { Container } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import { NavBar } from './components/Navigation';
+
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <DrawerAppBar />
-        <Routes>
-        <Route path="/" />
-        <Route path="/about" />
-        <Route path="/contact"  />
-        </Routes>
-      </Router>
-      <Container>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Welcome to ProductHub
-        </Typography>
-        <SampleButton />
-      </Container>
-    </div>
-  );
+    return (
+        <div>
+            <ThemeProvider theme={theme}>
+                <NavBar />
+                <h1>Hello, Material-UI</h1>
+                <SampleButton />
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
