65a5eaeffb24d6b27a5ca9967af28743df58b996
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-08-11 21:14:55 +0300

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { Navigation } from "./components/navigation/Navigation"
-import { Home } from "./components/home/Home"
-import { Footer } from "./components/footer/Footer"
-import { Copyright } from "./components/copyright/Copyright"
+import { Routes, Route } from 'react-router-dom';
+import { Navigation } from './components/navigation/Navigation';
+import { Home } from './components/home/Home';
+import { Footer } from './components/footer/Footer';
+import { Copyright } from './components/copyright/Copyright';
+import { About } from './components/about/About';
+import { Blog } from './components/blog/Blog';
+import { Coffees } from './components/coffees/Coffees';
+import { Contact } from './components/contact/Contact';
 
 function App() {
     return (
         <>
             <Navigation />
-            <Home />
-            <Footer />
-            <Copyright />
+            <main>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/blog' element={<Blog />} />
+                    <Route path='/cofees' element={<Coffees />} />
+                    <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <Footer />
+                <Copyright />
+            </main>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
