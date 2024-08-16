deb7a470a2059950e64d5f4fd02cb3c9ea6da829
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-08-16 22:38:41 +0300

    Added the authprovider in the main component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
 
+import { AuthProvider } from './context/auth-context.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>,
+	<StrictMode>
+		<AuthProvider>
+			<Router>
+				<App />
+			</Router>
+		</AuthProvider>
+	</StrictMode>,
 )
