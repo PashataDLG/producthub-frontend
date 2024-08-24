657e6a2606ff6377d80429ca384106e6894c2b6f
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-08-25 01:29:33 +0300

    fixed a typo

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App.jsx'
-
+import App from './App.jsx';
 import { AuthProvider } from './context/auth-context.jsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
 	<QueryClientProvider client={queryClient}>
-			<Router>
-				<AuthProvider>
-					<App />
-				</AuthProvider>
-			</Router>
+		<Router>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</Router>
 	</QueryClientProvider>
-)
+);
