cd893342d2f012d78a745dd5bf781ee05b698452
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-08-25 00:10:09 +0300

    added the react-query client

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.jsx'
 
 import { AuthProvider } from './context/auth-context.jsx';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<Router>
-			<AuthProvider>
-				<App />
-			</AuthProvider>
-		</Router>
-	</StrictMode>,
+	<QueryClientProvider client={queryClient}>
+			<Router>
+				<AuthProvider>
+					<App />
+				</AuthProvider>
+			</Router>
+	</QueryClientProvider>
 )
