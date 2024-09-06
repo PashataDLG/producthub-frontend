de43feec902fae9d81a17b70b78174c16dab7310
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-09-06 18:07:07 +0300

    added product provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.jsx';
 import { AuthProvider } from './context/auth-context.jsx';
+import { ProductProvider } from './context/product-context.jsx';
 
 const queryClient = new QueryClient();
 
@@ -10,7 +11,9 @@ createRoot(document.getElementById('root')).render(
 	<QueryClientProvider client={queryClient}>
 		<Router>
 			<AuthProvider>
-				<App />
+				<ProductProvider>
+					<App />
+				</ProductProvider>
 			</AuthProvider>
 		</Router>
 	</QueryClientProvider>
