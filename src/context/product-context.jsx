329ed440bd01b58b1ae8eb47c6bec55bd524a587
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-09-06 19:50:27 +0300

    Handler for adding products created in context

--- src/context/product-context.jsx
+++ src/context/product-context.jsx
@@ -1,19 +1,35 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
 import { createContext, useContext } from "react";
-import { useFetchProducts } from "../api/productApi";
+import { useFetchProducts, useAddProductMutation } from "../api/productApi";
+import { useNavigate } from "react-router-dom";
+
 
 const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
     const { data } = useFetchProducts();
+    const { mutateAsync: addProduct } = useAddProductMutation();
+
+    const navigate = useNavigate();
 
     const getProducts = () => {
         return data;
     }
 
+    const onAddProduct = async (productData) => {
+        try {
+            await addProduct(productData);
+            
+            navigate('/');
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     const productMethods = {
         getProducts,
+        onAddProduct
     };
 
     return (
