8856b7db36fafa7dc596bca8006917cfb87639b4
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-09-06 18:07:32 +0300

    home page now gets all products using the getProducts from context

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useProduct } from '../../context/product-context';
 
 import { AboutSection } from './about-section/AboutSection'
 import { BlogSection } from "./blog-section/BlogSection";
@@ -9,23 +9,20 @@ import { ContactSection } from "./contact-section/ContactSection";
 import { HeaderSection } from "./header-section/HeaderSection";
 
 export const Home = () => {
-    const [data, setData] = useState(null);
+    const { getProducts } = useProduct();
 
-    useEffect(() => {
-        fetch('https://serene-ocean-15581-68c8bef9ec28.herokuapp.com/api/products')
-            .then(res => res.json())
-            .then((resData) => setData(resData))
-            .catch(err => console.error('There was an error: ' + err));
-    }, []);
-    
+    const products = getProducts();
+
+    console.log(products);
+
     return (
         <>
-             <HeaderSection />
-             <CoffeeSection />
-             <AboutSection />
-             <ClientSection />
-             <BlogSection />
-             <ContactSection />
+            <HeaderSection />
+            <CoffeeSection />
+            <AboutSection />
+            <ClientSection />
+            <BlogSection />
+            <ContactSection />
         </>
     );
 };
