6e8d609377fb7cfe9e9fdb37964e0d4504b3e849
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-09-16 18:01:04 +0000

    created delete handler in the product context

--- src/context/product-context.jsx
+++ src/context/product-context.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
 import { createContext, useContext } from "react";
-import { useFetchProducts, useAddProductMutation } from "../api/productApi";
+import { useFetchProducts, useAddProductMutation, useDeleteProductMutation } from "../api/productApi";
 import { useNavigate } from "react-router-dom";
 
 
@@ -10,6 +10,7 @@ const ProductContext = createContext();
 export const ProductProvider = ({ children }) => {
     const { data } = useFetchProducts();
     const { mutateAsync: addProduct } = useAddProductMutation();
+    const { mutateAsync: deleteProduct } = useDeleteProductMutation();
 
     const navigate = useNavigate();
 
@@ -20,16 +21,28 @@ export const ProductProvider = ({ children }) => {
     const onAddProduct = async (productData) => {
         try {
             await addProduct(productData);
-            
+
             navigate('/products');
         } catch (error) {
             console.error(error);
         }
     }
 
+    const onDeleteProduct = async (productId) => {
+        try {
+            await deleteProduct(productId);
+
+            navigate('/products');
+        } catch (error) {
+            console.error(error);
+            
+        }
+    }
+
     const productMethods = {
         getProducts,
-        onAddProduct
+        onAddProduct,
+        onDeleteProduct
     };
 
     return (
